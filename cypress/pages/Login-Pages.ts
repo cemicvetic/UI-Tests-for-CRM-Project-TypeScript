4d7a0c0de5b133f38562c082cd3b507136d5f554
cemicvetic <cemicvetic@gmail.com>
2024-05-08 14:13:35 -0700

    Add Login-Pages.ts

---
+++ cypress/pages/Login-Pages.ts
@@ -0,0 +1,31 @@
+import BasePage from "./Base-Pages";
+
+class LoginPage extends BasePage {
+    get emailField() { return cy.get('#normal_login_email') }
+    get passwordField() { return cy.get('#normal_login_password') }
+    get buttonLogIn() { return cy.get('.login-form-button') }
+    get toast() { return cy.get('.ant-notification-notice-message') }
+
+
+    get emailValidationSign() { return cy.get('.ant-form-item-control-has-error').contains('Email') }
+    get passwordValidationSign() { return cy.get('.ant-form-item-control-has-error').contains('Password') }
+
+    get mainSign() { return cy.contains('h1') } // Example: Adjust according to actual text
+
+    get prescriptionAccount() { return cy.contains('p', "Don’t have an account?") }
+    get prescriptionAccountLink() { return cy.contains('a', "Create one") }
+    get prescriptionPassword() { return cy.contains('p', "Forgot your password?")}
+    get prescriptionPasswordLink() { return cy.contains('a', "Reset it")}
+
+    open() {
+        return super.open('/v5/user/login')
+    }
+
+    login(email, password) {
+        this.emailField.type(email);
+        this.passwordField.type(password);
+        this.buttonLogIn.click();
+    }
+}
+
+export default new LoginPage();
